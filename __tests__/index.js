4c295c4f2c4dfc599f0fc442c188e17da61bc1b4
twinh <twinhuang@qq.com>
2021-04-24 22:27:18 +0800

    test: 初始化测试

---
+++ __tests__/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LinkPicker from '..';
+import {render} from '@testing-library/react';
+
+describe('link picker', () => {
+  test('basic', () => {
+    const result = render(<LinkPicker/>);
+    expect(result.container.innerHTML).toMatchSnapshot();
+  });
+});
